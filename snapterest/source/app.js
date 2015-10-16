a9f92d22f262d5ec28d91cfde30c95ba6634ee33
Philip Damra <djheru@gmail.com>
2015-10-15 23:22:24 -0400

    Hello world

--- snapterest/source/app.js
+++ snapterest/source/app.js
@@ -1,8 +1,9 @@
 var React = require('react')
   , ReactDOM = require('react-dom');
 
-var h1 = React.createElement('h1', { className: 'header' }, 'This is React');
-var p = React.createElement('p', { className: 'content', key: 'content' }, 'And that\'s how it works');
-var reactFragment = [h1, p];
-var section = React.createElement('section', { className: 'container' }, reactFragment);
-ReactDOM.render(section, document.getElementById('react-application'));
+var listItemElement1 = React.createElement('li', {className: 'item-1', key: 'item-1'}, 'Item 1');
+var listItemElement2 = React.createElement('li', {className: 'item-2', key: 'item-2'}, 'Item 2');
+var listItemElement3 = React.createElement('li', {className: 'item-3', key: 'item-3'}, 'Item 3');
+var reactFragment = [listItemElement1, listItemElement2, listItemElement3];
+var listOfItems = React.createElement('ul', {className: 'list-of-items'}, reactFragment);
+ReactDOM.render(listOfItems, document.getElementById('react-application'));
