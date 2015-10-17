1bed5814780df55edc6dd4763c002de3ec20a85c
Philip Damra <djheru@gmail.com>
2015-10-16 23:23:20 -0400

    Using JSX

--- snapterest/build/snapterest.js
+++ snapterest/build/snapterest.js
@@ -18670,12 +18670,34 @@ var listItemElement3 = createListItemElement({className: 'item-3', key: 'item-3'
 var reactFragment = [listItemElement1, listItemElement2, listItemElement3];
 var listOfItems = React.createElement('ul', {className: 'list-of-items'}, reactFragment);*/
 
-var listItemElement1 = React.DOM.li({ className: 'item-1', key: 'item-1' }, 'Item-1');
-var listItemElement2 = React.DOM.li({ className: 'item-2', key: 'item-2' }, 'Item-2');
-var listItemElement3 = React.DOM.li({ className: 'item-3', key: 'item-3' }, 'Item-3');
+/*
+var listItemElement1 = React.DOM.li({className: 'item-1', key: 'item-1'}, 'Item-1');
+var listItemElement2 = React.DOM.li({className: 'item-2', key: 'item-2'}, 'Item-2');
+var listItemElement3 = React.DOM.li({className: 'item-3', key: 'item-3'}, 'Item-3');
 
 var reactFragment = [listItemElement1, listItemElement2, listItemElement3];
-var listOfItems = React.createElement('ul', { className: 'list-of-items' }, reactFragment);
+var listOfItems = React.createElement('ul', {className: 'list-of-items'}, reactFragment);
+*/
+
+var listOfItems = React.createElement(
+  'ul',
+  { className: 'list-of-items' },
+  React.createElement(
+    'li',
+    { className: 'item-1' },
+    'Item 1'
+  ),
+  React.createElement(
+    'li',
+    { className: 'item-2' },
+    'Item 2'
+  ),
+  React.createElement(
+    'li',
+    { className: 'item-3' },
+    'Item 3'
+  )
+);
 ReactDOM.render(listOfItems, document.getElementById('react-application'));
 
 },{"react":155,"react-dom":2}]},{},[156]);
